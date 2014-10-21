6eda84bc01b7d74dab08295bfcf8a45757ff40ae
syo-sa1982 <fujii1308@gmail.com>
2014-10-21 22:59:17 +0900

    一旦scriptからボール生成 * 次はランダムで色指定したい

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -18,12 +18,21 @@ public class Ball
 
 public class BallControl : MonoBehaviour 
 {
+	private Color color;
 
-	public Ball.COLOR color = (Ball.COLOR)0;
+	void Awake()
+	{
+		int random = (int)Random.Range (0.0f,5.0f);
+		Debug.Log ("BallControl Awake");
+		Debug.Log (random);
+	}
 
 	// Use this for initialization
 	void Start () 
 	{
+//		GameObject ball = Instantiate (Resources.Load ("Prefabs/Ball"),new Vector3(0,5,0),Quaternion.identity) as GameObject;
+//		GameObject ball = Instantiate (Resources.Load ("Prefabs/Ball")) as GameObject;
+
 	
 	}
 	
@@ -35,30 +44,30 @@ public class BallControl : MonoBehaviour
 
 	public void setColor(Ball.COLOR color) 
 	{
-		this.color = color;
-
-		Color color_value;
-
-		switch(this.color) 
-		{
-		default:
-		case Ball.COLOR.BULE:
-			color_value = Color.blue;
-			break;
-		case Ball.COLOR.YELLOW:
-			color_value = Color.yellow;
-			break;
-		case Ball.COLOR.GREEN:
-			color_value = Color.green;
-			break;
-		case Ball.COLOR.ORANGE:
-			color_value = new Color(1.0f, 0.46f, 0.0f);
-			break;
-		case Ball.COLOR.RED:
-			color_value = Color.red;
-			break;
-		}
-
-		this.renderer.material.color = color_value;
+//		this.color = color;
+//
+//		Color color_value;
+//
+//		switch(this.color) 
+//		{
+//		default:
+//		case Ball.COLOR.BULE:
+//			color_value = Color.blue;
+//			break;
+//		case Ball.COLOR.YELLOW:
+//			color_value = Color.yellow;
+//			break;
+//		case Ball.COLOR.GREEN:
+//			color_value = Color.green;
+//			break;
+//		case Ball.COLOR.ORANGE:
+//			color_value = new Color(1.0f, 0.46f, 0.0f);
+//			break;
+//		case Ball.COLOR.RED:
+//			color_value = Color.red;
+//			break;
+//		}
+//
+//		this.renderer.material.color = color_value;
 	}
 }
--- Assets/Scripts/BallRoot.cs
+++ Assets/Scripts/BallRoot.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class BallRoot : MonoBehaviour 
 {
 
-	public GameObject ballSpawnPoint;
+	public GameObject ballPrefab;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		Debug.Log ("BallRoot Start");
+
+		Instantiate (ballPrefab, ballPrefab.transform.position, ballPrefab.transform.rotation);
 	}
 	
 	// Update is called once per frame
@@ -23,11 +25,11 @@ public class BallRoot : MonoBehaviour
 	{
 		for (var i = 0; i < number; i++)
 		{
-			GameObject ball = ChoosePrehab();
-			Vector3 position = ballSpawnPoint.transform.position;
-			position.x = Random.Range(position.x - 1.2f, position.x + 1.2f);
-			position.y = Random.Range(position.y - 1.0f, position.y + 1.3f);
-			Instantiate(ball, position, ball.transform.rotation);
+//			GameObject ball = ChoosePrehab();
+//			Vector3 position = ballSpawnPoint.transform.position;
+//			position.x = Random.Range(position.x - 1.2f, position.x + 1.2f);
+//			position.y = Random.Range(position.y - 1.0f, position.y + 1.3f);
+//			Instantiate(ball, position, ball.transform.rotation);
 		}
 	}
 }
