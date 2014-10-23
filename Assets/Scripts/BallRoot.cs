94af228043ca526ed952e8a656f9c06ec90ea5db
syo-sa1982 <fujii1308@gmail.com>
2014-10-23 09:31:50 +0900

    再帰関数にした

--- Assets/Scripts/BallRoot.cs
+++ Assets/Scripts/BallRoot.cs
@@ -11,31 +11,30 @@ public class BallRoot : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		Debug.Log ("BallRoot Start");
-
 		PrepareBalls (ballNum);
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
 	}
-
 
 	/**
 	 * ボールを生成
 	 */
 	void PrepareBalls(int number)
 	{
-		for (var i = 0; i < number; i++)
-		{
-			Vector3 position = ballPrefab.transform.position;
-			position.x = Random.Range(position.x - 1.2f, position.x + 1.2f);
-			position.y = Random.Range(position.y - 1.0f, position.y + 1.3f);
-			Instantiate(ballPrefab, position, ballPrefab.transform.rotation);
+		if (number <= 0) {
+			return;
 		}
+
+		Vector3 position = ballPrefab.transform.position;
+		position.x = Random.Range(position.x - 1.2f, position.x + 1.2f);
+		position.y = Random.Range(position.y - 1.0f, position.y + 1.3f);
+		Instantiate(ballPrefab, position, ballPrefab.transform.rotation);
+
+		// numberが０になるまで再帰
+		--number;
+		PrepareBalls (number);
 	}
 }
