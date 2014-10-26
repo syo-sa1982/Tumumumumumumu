bf3795248a3413fc440c4de9150c9942379b3a3b
syo-sa1982 <fujii1308@gmail.com>
2014-10-26 22:37:09 +0900

    3Dオブジェクトで生成してたパズルピースをSpriteに変えた。 * 結果的にコード量減った

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,68 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-public class Ball
-{
-	public enum COLOR
-	{
-		WHITE = 0,
-		BULE,
-		YELLOW,
-		GREEN,
-		ORANGE,
-		RED,
-		NUM,
-	};
-}
-
 public class BallControl : MonoBehaviour 
 {
-	private Ball.COLOR color;
-
-	void Awake()
-	{
-		int color_index = (int)Random.Range (0.0f,6.0f);
-		setColor ((Ball.COLOR)color_index);
-	}
+	const string FILE_NAME = "Sprites/TumuYatsu";
+	const string SPRITE_NAME = "TumuYatsu_";
 
 	// Use this for initialization
 	void Start () 
 	{
+		string name = SPRITE_NAME + Random.Range(0, 5);
+		Sprite sp = GetSprite(FILE_NAME, name);
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		sr.sprite = sp;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
 	}
 
-	public void setColor(Ball.COLOR color) 
+	public Sprite GetSprite(string fileName, string spriteName)
 	{
-		this.color = color;
-
-		Color color_value;
-
-		switch(this.color) 
-		{
-		default:
-		case Ball.COLOR.BULE:
-			color_value = Color.blue;
-			break;
-		case Ball.COLOR.YELLOW:
-			color_value = Color.yellow;
-			break;
-		case Ball.COLOR.GREEN:
-			color_value = Color.green;
-			break;
-		case Ball.COLOR.ORANGE:
-			color_value = new Color(1.0f, 0.46f, 0.0f);
-			break;
-		case Ball.COLOR.RED:
-			color_value = Color.red;
-			break;
-		}
-
-		this.renderer.material.color = color_value;
+		Sprite[] sprites = Resources.LoadAll<Sprite>(fileName);
+		return System.Array.Find<Sprite>(sprites, (sprite) => sprite.name.Equals(spriteName));
 	}
 }
